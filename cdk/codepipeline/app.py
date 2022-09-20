71b3d5962ecc560950664f9a3829b2346d6dbcc2
Focuszaa <Focuszaaphoto@gmail.com>
2022-09-20 23:16:15 +0700

    feat(cw_event): add eventrigger stack

--- cdk/codepipeline/app.py
+++ cdk/codepipeline/app.py
@@ -2,6 +2,8 @@ from aws_cdk import (App)
 
 from Base import Base
 from Pipeline import Pipeline
+from EventTrigger import EventTrigger
+# from cdk.codepipeline.EventTrigger import EventTrigger
 
 props = {'projectName': 'ImageBuilder'}
 
@@ -14,4 +16,9 @@ base = Base(app, f"{props['projectName']}-base", props)
 # pipeline stack 
 pipeline = Pipeline(app, f"{props['projectName']}-pipeline", base.outputs)
 pipeline.add_dependency(base)
+
+#ventRule stacks
+eventrule = EventTrigger(app, f"{props['projectName']}-event-rule", pipeline.outputs)
+eventrule.add_dependency(pipeline)
+
 app.synth()
