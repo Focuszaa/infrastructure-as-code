362033bf9d46ca7bea3eab18daba815889e24467
Focuszaa <Focuszaaphoto@gmail.com>
2022-09-21 23:38:40 +0700

    chore(cw event) : 🧽 cleanup and add ourput

--- cdk/codepipeline/EventTrigger.py
+++ cdk/codepipeline/EventTrigger.py
@@ -1,17 +1,18 @@
 from aws_cdk import (
-    # aws_cloudwatch as cw,
     aws_events as ev,
     aws_codecommit as cc,
     aws_iam as iam,
     aws_events_targets as targets,
-    App, Aws, CfnOutput, Duration, RemovalPolicy, Stack
+    App, CfnOutput, Stack
 )
+
 
 class EventTrigger(Stack):
     def __init__(self, app: App, id: str, props, **kwargs) -> None:
         super().__init__(app, id, **kwargs)
-        
-        cw_event_service_role = iam.Role(self, "Role",
+
+        cw_event_service_role = iam.Role(
+            self, "Role",
             assumed_by=iam.ServicePrincipal("events.amazonaws.com"),
             description="Event-service-role",
             inline_policies={
@@ -26,37 +27,54 @@ class EventTrigger(Stack):
                 )
             }
         )
-        
+
         # cc means codecommit
-        cc_change_rule =ev.Rule(self, "cc_change_rule",
+        cc_change_rule = ev.Rule(
+            self, "cc_change_rule",
             event_pattern=ev.EventPattern(
-              source=["aws.codecommit"],
-              detail_type=['CodeCommit Repository State Change'],
-              resources=[props['cc_repo'].repository_arn],
-              detail={
-                "event": ["referenceCreated","referenceUpdated"],
-                "referenceType": "branch",
-                "referenceName": "main"
-              }
+                source=["aws.codecommit"],
+                detail_type=['CodeCommit Repository State Change'],
+                resources=[props['cc_repo'].repository_arn],
+                detail={
+                    "event": ["referenceCreated", "referenceUpdated"],
+                    "referenceType": "branch",
+                    "referenceName": "main"
+                }
             )
         )
-        
+
         cc_change_rule.add_target(targets.CodePipeline(
-          props['cp'],
-          event_role=cw_event_service_role,
-          retry_attempts=2
+            props['cp'],
+            event_role=cw_event_service_role,
+            retry_attempts=2
         ))
-        
+
         # cw means codewatch
-        cw_schedule_weekly_rule =ev.Rule(
-          self, "cw_schedule_weekly_rule",
-          rule_name='WeeklyEventSchedule',
-          description='Build once a week on Monday random time ~ 8-10 AM',
-          schedule=ev.Schedule.expression("cron(0 1-5 ? * MON *)")
+        cw_schedule_weekly_rule = ev.Rule(
+            self, "cw_schedule_weekly_rule",
+            rule_name='WeeklyEventSchedule',
+            description='Build once a week on Monday random time ~ 8-10 AM',
+            schedule=ev.Schedule.expression("cron(0 1-5 ? * MON *)")
         )
-        
+
         cw_schedule_weekly_rule.add_target(targets.CodePipeline(
-          props['cp'],
-          event_role=cw_event_service_role,
-          retry_attempts=2
+            props['cp'],
+            event_role=cw_event_service_role,
+            retry_attempts=2
         ))
+        
+        CfnOutput(
+            self, "Output-Event-service-role",
+            description="Event-service-role",
+            value=cw_event_service_role.role_arn
+        )
+        CfnOutput(
+            self, "Output-cc_change_rule",
+            description="codecommit change event rule",
+            value=cc_change_rule.rule_arn
+        )
+        CfnOutput(
+            self, "Output-cw_schedule_weekly_rule",
+            description="schedule weekly event rule",
+            value=cw_schedule_weekly_rule.rule_arn
+        )
